361ea4c6c6426faa1f9e02937e04b0a93e9f2bef
Mark Bitancor <marksantos.bitancor@bicol-u.edu.ph>
2023-05-11 20:01:38 +0800

    added reg page with backend

---
+++ html/reg.php
@@ -0,0 +1,38 @@
+<?PHP
+    $name = $_GET['rname'];
+    $email = $_GET['remail'];
+    $pass1 = $_GET['renterPass'];
+    $pass2 = $_GET['rconfirmPass'];
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "reg";
+    $con = mysqli_connect($servername,$username,$password,$dbname);
+    if(!$con){
+        die("Error : ".mysqli_connect_error());
+    }
+    if($pass1 == $pass2){
+        $sql = "INSERT INTO `registration`(`name`, `email`, `password`) VALUES('$name','$email','$pass1');";
+        if(mysqli_query($con,$sql)){
+            echo '<script>
+                window.location.href = "registration.html";
+                alert("Registration Successful!")
+            </script>';
+        }
+        else{
+            echo '<script>
+                window.location.href = "registration.html";
+                alert("Something Went Wrong...")
+            </script>';
+        }
+    }
+    else {
+        echo '<script>
+            window.location.href = "registration.html";
+            alert("Password Not Matched!")
+        </script>';
+    }
+
+
+?>
